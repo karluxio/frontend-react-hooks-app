19fd9ac5b7b6cab9ee4e6edea18d3d1c17f0cd51
lumusitech <lumusitech@gmail.com>
2023-04-30 18:02:51 -0300

    feat: apply React.Memo and useCallback

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import ReactDOM from 'react-dom/client'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memorize } from './06-memos/Memorize'
-import { MemoHook } from './06-memos/MemoHook'
+// import { MemoHook } from './06-memos/MemoHook'
+import { CallbackHook } from './06-memos/CallbackHook'
 
 import './index.css'
 
@@ -26,7 +27,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <FocusScreen /> */}
     {/* <Layout /> */}
     {/* <Memorize /> */}
-    <MemoHook />
+    {/* <MemoHook /> */}
+    <CallbackHook />
   </>
 
 
